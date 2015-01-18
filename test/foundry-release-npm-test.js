80edb31b2c170a231c5a5a6446a05cecafe17b8e
Todd Wolfson <todd@twolfson.com>
2015-01-18 14:00:24 -0600

    Cleaning up code

--- test/foundry-release-npm-test.js
+++ test/foundry-release-npm-test.js
@@ -1,12 +1,7 @@
 // Load in dependencies
-var fs = require('fs');
 var expect = require('chai').expect;
-var sinon = require('sinon');
-var shell = require('shelljs');
-var npmRelease = require('../');
+var npmReleaseBuild = require('../');
 var fixtureUtils = require('./utils/fixtures');
-
-// TODO: Relocate utils into spec
 
 // Guarantee safeguards against exec are in place (see WARNING.md)
 var childUtils = require('./utils/child-process');
@@ -19,47 +14,13 @@ var initialParams = {
 };
 describe('Updating files', function () {
   describe('for a node module', function () {
-    var fixtureDir = fixtureUtils.fixtureDir('npm');
+    var fixtureDir = fixtureUtils.fixtureDir('npm-build');
     before(function updateFiles (done) {
-      npmRelease.updateFiles(initialParams, done);
+      npmReleaseBuild.updateFiles(initialParams, done);
     });
 
-    it('updates the package.json', function () {
-      var pkgJson = fs.readFileSync(fixtureDir + '/package.json');
+    it('builds the package', function () {
       expect(JSON.parse(pkgJson)).to.have.property('version', '0.1.0');
-    });
-
-  });
-});
-
-describe('Publishing', function () {
-  describe('a node module', function () {
-    var fixtureDir = fixtureUtils.fixtureDir('npm');
-    before(function (done) {
-      this.execStub = sinon.stub(shell, 'exec');
-      npmRelease.publish(initialParams, done);
-    });
-    after(function () {
-      this.execStub.restore();
-    });
-
-    it('publishes to npm', function () {
-      expect(this.execStub.args[0]).to.deep.equal(['npm publish']);
-    });
-  });
-
-  describe('a private node module', function () {
-    var fixtureDir = fixtureUtils.fixtureDir('npm-private');
-    before(function (done) {
-      this.execStub = sinon.stub(shell, 'exec');
-      npmRelease.publish(initialParams, done);
-    });
-    after(function () {
-      this.execStub.restore();
-    });
-
-    it('publishes to npm', function () {
-      expect(this.execStub.args).to.have.property('length', 0);
     });
   });
 });
